b588f9492897184d978612281cadb59c8108f7e2
Deepak3015 <D03748651@gmail.com>
2025-06-20 15:45:22 +0530

    New commit

--- Prep/Program.cs
+++ Prep/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("How Many values you want to be in you array :");
+        int num = int.Parse(Console.ReadLine());
+
+        int[] numbers = new int[num];
+        for (int i = 0; i < num; i++)
+        {
+            Console.WriteLine("Enter your Data :");
+            int Data = int.Parse(Console.ReadLine());
+            numbers[i] = Data;
+
+        }
+        Console.WriteLine("==========Your Data===========");
+        foreach (int item in numbers)
+        {
+            Console.WriteLine(item);
+        }
+    
+
+    }
+  
+}
